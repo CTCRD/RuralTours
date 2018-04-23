ec9494da509df8a8e2a3653c30ad8d00829e152f
cuzox <calevime@gmail.com>
2018-04-23 12:23:29 -0400

    Fixed navigation

--- js/router.js
+++ js/router.js
@@ -5,7 +5,7 @@ var Router = (() =>{
   }
 
   function reload(){
-    let hash = window.location.hash.substr(1).split('/')
+    let hash = window.location.pathname.substr(1).split('/')
     if (callbacks[hash[0]]) callbacks[hash[0]]()
     else {
       window.location.hash = ''
@@ -16,17 +16,17 @@ var Router = (() =>{
   reload()
 
   function params(){
-    let hash = window.location.hash.substr(1).split('/')
+    let hash = window.location.pathname.substr(1).split('/')
     if(hash.length > 1) return hash.slice(1)
     else return []
   }
 
   function go(hash){
-    history.pushState(null, null, '#' + (hash == 'home' || !hash ? '' : hash));
+    history.pushState(null, null, (hash == 'home' || !hash ? '/' : hash));
     reload()
   }
   
-  window.onhashchange = ()=>{
+  window.onpopstate = ()=>{
     Router.reload()
   }
 
